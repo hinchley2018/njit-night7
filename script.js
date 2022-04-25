482b5c49e3707ad2a6a424d34c00531193ae0553
Jonathon Hinchley <jonathon.hinchley18@gmail.com>
2022-04-25 17:42:07 -0500

    dom manipulation part 1

---
+++ script.js
@@ -0,0 +1,38 @@
+console.log("Script loaded")
+
+//get the h2
+//tag
+//#id in css
+//.class in css
+//[attribute] in css
+let courseHeader = document.querySelector("#main-header")
+
+//have the ref set text content 
+courseHeader.textContent ="NJIT Department of Computer Science"
+
+let course1 = {
+    department: "CSCE",
+    courseName: "CSCE-121: Fundamentals of Programming",
+    requiredClassYear: 1,
+    available_spots: 0
+}
+
+let course2 = {
+    department: "CSCE",
+    courseName: "CSCE-310: Operating Systems",
+    requiredClassYear: 3,
+    available_spots: 3
+}
+
+let available_courses = [course1, course2]
+
+
+//select all elements
+let courseListItems = document.querySelectorAll(".course")
+console.log("my course lis", courseListItems)
+
+//green means there are available spots
+for (let index = 0; index < courseListItems.length; index++) {
+    courseListItems[index].style.backgroundColor = "green"
+
+}
